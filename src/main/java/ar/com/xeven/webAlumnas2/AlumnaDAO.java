8646f6f47c4089acd1cc0754c1b5037c3ef719bc
pabloacvd <pablo@xeven.com.ar>
2021-05-15 12:59:24 -0300

    Initial commit

---
+++ src/main/java/ar/com/xeven/webAlumnas2/AlumnaDAO.java
@@ -0,0 +1,25 @@
+package ar.com.xeven.webAlumnas2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AlumnaDAO {
+    public static List<Alumna> getAlumnas() {
+        List<Alumna> alumnas = new ArrayList<>();
+
+        alumnas.add(new Alumna("natalia","back5"));
+        alumnas.add(new Alumna("sabri","back5"));
+        alumnas.add(new Alumna("juli","front8"));
+        alumnas.add(new Alumna("agus","back5"));
+        alumnas.add(new Alumna("anto","front8"));
+        alumnas.add(new Alumna("alba","front8"));
+        alumnas.add(new Alumna("brigitte","front8"));
+        alumnas.add(new Alumna("eve","front8"));
+        alumnas.add(new Alumna("vane","back5"));
+        alumnas.add(new Alumna("libia","front8"));
+        alumnas.add(new Alumna("justyne","back5"));
+
+
+        return alumnas;
+    }
+}
